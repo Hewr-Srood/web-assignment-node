146e5fe1571af1baea501f2ce4759d91a6250c01
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2021-11-20 20:58:42 +0300

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,1 +1,14 @@
 
+const mongoose = require("mongoose");
+
+const reqString = {
+  type: String,
+  required: true,
+};
+const userSchema = new mongoose.Schema({
+  username: reqString,
+  password: reqString,
+});
+
+const user = mongoose.model("user", userSchema);
+module.exports = user;
